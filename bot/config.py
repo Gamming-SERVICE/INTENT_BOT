82f96d97feec1442ff36272884e582ff77c2ac70
GAMMING-SERVICE  [SYSTEM NODE-01] <167754757+Gamming-SERVICE@users.noreply.github.com>
2026-02-23 00:15:15 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,14 @@
+import os
 
+DEFAULT_PREFIX = "!"
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+LAVALINK_URI = os.getenv("LAVALINK_URI")
+LAVALINK_PASSWORD = os.getenv("LAVALINK_PASSWORD")
+
+INTENTS = {
+    "members": True,
+    "message_content": True,
+    "guilds": True,
+}
