b092e5a01c65b5c7c059dd39c0e322398f33fbdc
GAMMING-SERVICE  [SYSTEM NODE-01] <167754757+Gamming-SERVICE@users.noreply.github.com>
2026-02-23 00:15:52 +0530

    Update embeds.py

--- bot/utils/embeds.py
+++ bot/utils/embeds.py
@@ -1,1 +1,11 @@
+import discord
+import datetime
 
+def create_embed(title, description=None, color=discord.Color.blurple()):
+    embed = discord.Embed(
+        title=title,
+        description=description,
+        color=color,
+        timestamp=datetime.datetime.utcnow()
+    )
+    return embed
