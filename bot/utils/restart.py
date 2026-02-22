01033e76ae1acd9fcd1ab647d33e8b7b54bc254a
GAMMING-SERVICE  [SYSTEM NODE-01] <167754757+Gamming-SERVICE@users.noreply.github.com>
2026-02-22 19:18:40 +0530

    Update restart.py

--- bot/utils/restart.py
+++ bot/utils/restart.py
@@ -1,1 +1,12 @@
+import asyncio
+import logging
+import sys
 
+async def graceful_crash():
+    logging.error("Bot crashed. Restarting in 10 seconds...")
+
+    for i in range(10, 0, -1):
+        logging.error(f"Restarting in {i}...")
+        await asyncio.sleep(1)
+
+    sys.exit(1)
